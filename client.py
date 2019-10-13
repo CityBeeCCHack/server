aac903431a2e5eb89488ec6a552da2546c4c810f
MelonBomberAsh <56116173+MelonBomberAsh@users.noreply.github.com>
2019-10-13 03:37:02 +0100

    New features

--- client.py
+++ client.py
@@ -8,34 +8,41 @@ except ImportError:
     from smbus import SMBus
 from bme280 import BME280
 
-localIP = "192.168.137.1"
+serverIP = "192.168.88.161"
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
 
+flag = 0
 while True:
 
     temperature = bme280.get_temperature()
     pressure = bme280.get_pressure() * 100
     humidity = bme280.get_humidity()
-    sensorPayload =  {"Time": str(int(time.time())),
-                "Temperature": f"{temperature:.1f}",
-                "Pressure": f"{pressure:.1f}",
-                "Humidity": f"{humidity:.1f}"}
+    sensorPayload =  {"Time": int(time.time()),
+                "Temperature": int(temperature),
+                "Pressure": int(pressure),
+                "Humidity": int(humidity)}
+    while True:
+        try:
+            response = requests.post(f'http://{serverIP}:1337/pi/sensors', json=sensorPayload)
+            print(f"Successfully sent sensor data: {str(sensorPayload)}")
+            break
+        except:
+            print("Could not connect to server")
+            time.sleep(30)
+    
 
-    response = requests.post(f'http://{localIP}:1337/pi', json=sensorPayload)
-    if response.ok:
-        print(f"Successfully sent sensor data: {str(sensorPayload)}")
-    else:
-        print(f"Sensor data failed to send: {response.reason}")
+    if flag == 0:
+        subprocess.call(["raspistill", "-n", "-t", "1", "-q", "50", "-o", r"/home/pi/CityBee/image1.jpg"])
+        time.sleep(0.5)
+        with open(r"/home/pi/CityBee/image1.jpg", "rb") as imageFile:
+            response = requests.post(f'http://{serverIP}:1337/pi/image', data=imageFile)
+            if response.ok:
+                print("Successfully sent image")
+            else:
+                print(f"Image failed to send: {response.reason}")
+        flag += 0
 
-    subprocess.call(["raspistill", "-n", "-t 1", "-q 50", "-o ~/PlanBee/image.jpg"])
-    with open("\CityBee\image.jpg", "rb") as imageFile:
-        response = requests.post(f'http://{localIP}:1337/pi/image', data=imageFile)
-        if response.ok:
-            print("Successfully sent image")
-        else:
-            print(f"Image failed to send: {response.reason}")
-
-        time.sleep(10)
+    time.sleep(60)
 
 
--- server.py
+++ server.py
@@ -1,16 +1,59 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
+from PIL import Image
 
 app = Flask(__name__)
-localIP = "192.168.137.1"
+port = 1337
 
-@app.route('/pi', methods=['POST'])
-def pi():
+@app.route('/pi/sensors', methods=['POST'])
+def saveSensorData():
     sensorData = request.json
-    print(f'Value on server {sensorData}')  # --> Value on server {'temp': 100, 'temp_1': 150}
-    with open("output.jpg", "wb") as outputFile:
-        outputFile.write(request.data)
+    print(f"Sensor data received: {str(sensorData)}")
+    with open("Sensor Logs.csv", "a") as csvFile:
+        values = ",".join(str(value) for value in sensorData.values())
+        csvFile.write(values + "\n")
     return jsonify(sensorData)
 
+@app.route('/pi/image', methods=['POST'])
+def image():
+    with open("feed.jpg", "wb") as outputFile:
+        outputFile.write(request.data)
+        return("Image received")
+
+@app.route('/disconnect')
+def disconnect():
+    return {"Name": "John Doe", "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+
+@app.route('/disconnect/sensors', methods=["GET"])
+def sendSensorData():
+    with open("Sensor Logs.csv", "r") as csvFile:
+        # content = csvFile.read()
+        parameters = dict(request.args)
+        if "quantity" in parameters:
+            quantity = int(request.args.get("quantity"))
+            content = csvFile.read().split("\n")
+            desiredData = content[-(quantity+1):]
+            return "\n".join(desiredData)
+        else:
+            return csvFile.read()
+        return content
+
+@app.route('/disconnect/feed')
+def feed():
+    return send_from_directory("", "feed.jpg")
+
+@app.route('/disconnect/sucrose')
+def sucrose():
+    img = Image.open('feed.jpg')
+    # img.show()
+    pix = img.load()
+    print(img.size)
+    counter = 0
+    for y in range(img.size[1]):
+        for x in range(img.size[0]):
+            if all(i>150 for i in pix[x,y]):
+                counter += 1
+    ratio = counter/(img.size[1] * img.size[0]) * 100
+    return f"{ratio:.1f}"
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", debug=True, port = 1337)
+    app.run(host = "0.0.0.0", port = port)
